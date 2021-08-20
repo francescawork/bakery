9fe6b0abd1532f7dea20913bbe6961983c2d61cb
Terence <therealterencequ@gmail.com>
2021-08-20 23:13:43 +1200

    comment detailing program specification

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+# a bakery includes the product and the customers
+# the product is the thing you sell
+# -- The product can sell for a different price
+# the customers is who is going to buy the product
+# -- What the customers are willing to pay for
+# Do a probably study for bakery
 def main():
     print("Test")
 
 
 if __name__ == "__main__":
     main()
-
